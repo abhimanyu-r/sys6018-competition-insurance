51229638e0b5e2b021b28538a8e961a2474cc872
Gregory A. Wert <gaw8pa@virginia.edu>
2017-11-03 09:52:30 -0400

    Add files via upload

--- SVM.R
+++ SVM.R
@@ -5,27 +5,43 @@ source("Gini.R")
 source("preprocess.R")
 library(e1071)
 
+# Load in the Train Data
 train = fill_missing(read.csv('train.csv'))
 for(name in colnames(train)){
   if(str_detect(name, "cat")){
     train[[name]] <- as.factor(train[[name]])}}
 
+# Stratify the data into a smaller sample
+train.data = stratified.sample(train,50000)
 
-train.data = stratified.sample(train,10000)
-train.data$id = NULL
-
-#------------------------------------------
-#POLY
-svm.model.poly = svm(target~.,data=train.data,kernel='polynomial',degree=3,cost=1,na.action = na.exclude)
-
-summary(svm.model.poly)
-
-test = read.csv('test.csv')
+# Load in the test data
+test = fill_missing(read.csv('test.csv'))
 for(name in colnames(test)){
   if(str_detect(name, "cat")){
     test[[name]] <- as.factor(test[[name]])}}
-preds = predict(svm.model.poly,test[2:58])
 
 #------------------------------------------
-#RAD
+#POLYNOMIAL KERNEL
+set.seed(1)
+
+# generate the model
+svm.model.poly = svm(target~.-id,data=train.data,kernel='polynomial',degree=3,cost=1)
+summary(svm.model.poly)
+
+# generate predictions
+set.seed(1)
+preds = predict(svm.model.poly,test, type = 'response')
+preds[preds<0] = 0
+preds[preds>1] = 1
+
+# cross validate
+tune.svm(target~.,data=train.data,kernel='polynomial')
+
+write.csv(data.frame(id=as.integer(test$id),target=preds),'svm_submissions.csv', row.names = FALSE)
+
+#------------------------------------------------------------------------
+#RADIAL KERNEL
 svm.model.rad = svm(target~.,data=train.data,kernel='radial',gamma=1,cost=1)
+
+tune.svm(target~.,data=train.data,kernel='radial',gamma=1,cost=1)
+# the polynonmial performed better
